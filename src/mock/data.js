31fad497ce33e60e97b9792fd7f99d07c1db60e7
Norman Wong <nich19032@outlook.com>
2020-05-14 14:30:15 +1000

    Updated Hero, Head and About Data

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,33 +2,44 @@ import uuidv1 from 'uuid/v1';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
-  lang: '', // e.g: en, es, fr, jp
-  description: '', // e.g: Welcome to my website
+  title: 'Norman Wong Chiew Look | AWS Cloud Engineer ', // e.g: 'Name | Developer'
+  lang: 'en', // e.g: en, es, fr, jp
+  description: 'Norman Wong Website', // e.g: Welcome to my website
 };
 
 // HERO DATA
 export const heroData = {
-  title: '',
-  name: '',
-  subtitle: '',
-  cta: '',
+  title: 'Hi, my name is',
+  name: 'Norman',
+  subtitle: "I'm a AWS Cloud Engineer",
+  cta: 'Know more',
 };
 
 // ABOUT DATA
 export const aboutData = {
   img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  paragraphOne:
+    // eslint-disable-next-line no-multi-str
+    'Recent graduate with 1+ year of work experience \
+    in two different Australian start-ups (Treiner and Shifra), \
+    where I had hands on experience designing and building out AWS cloud networks and infrastructures \
+    for Development, Staging and Production environment that are needed for existing Client-side SPA \
+    or Server-side rendered (SSR) web application(s).',
+  paragraphTwo:
+    // eslint-disable-next-line no-multi-str
+    'I have a broad knowledge and hands-on experience in software development practices \
+  (Agile, CI/CD, Continuous Deployment, secret management, Girflow workflow), Linux system administration, \
+  network administration, database administration, security and storage in the AWS cloud. ',
+  paragraphThree:
+    "I'm basically the operations / infrastructure person who write code to automate as much manual process / work as possible.",
+  resume: 'https://www.nich19302.com/NormanWongCL_Resume.pdf', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
 export const projectsData = [
   {
     id: uuidv1(),
-    img: 'project.jpg',
+    img: 'project1.jpg',
     title: '',
     info: '',
     info2: '',
@@ -57,9 +68,9 @@ export const projectsData = [
 
 // CONTACT DATA
 export const contactData = {
-  cta: '',
-  btn: '',
-  email: '',
+  cta: 'Want to Work Together? Awesome!',
+  btn: 'Contact Me',
+  email: 'nich19032@outlook.com',
 };
 
 // FOOTER DATA
