2d5587013573edc41e3a05e26e3cc2cf6570a20a
Norman Wong <nich19032@outlook.com>
2020-05-14 14:58:44 +1000

    Updated project section

--- src/mock/data.js
+++ src/mock/data.js
@@ -39,30 +39,34 @@ export const aboutData = {
 export const projectsData = [
   {
     id: uuidv1(),
+    img: 'project2.jpg',
+    title: 'Sep 2019 – Dec 2019, Treiner',
+    info:
+      // eslint-disable-next-line no-multi-str
+      'Took part in designing and implementing the AWS network and infrastructure \
+      for Staging and Production environment to support an MVC monolithic Laravel Application.',
+    info2: '',
+    url: 'https://treiner.co/',
+  },
+  {
+    id: uuidv1(),
     img: 'project1.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    title: 'Feb 2018 – Oct 2018, Shifra',
+    info:
+      // eslint-disable-next-line no-multi-str
+      'Responsible for architecting and building out an automated CI/CD pipeline \
+      and AWS infrastructure for Development and Production Environment.',
+    info2: 'And Taking part in Web Development work when needed.',
+    url: 'https://shifra.app',
   },
   {
     id: uuidv1(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
-  },
-  {
-    id: uuidv1(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'project3.jpg',
+    title: 'Dualstack IPv4/IPv6 VPC network (Work in Progress)',
+    info:
+      'AWS cloudformation templates for creating a dualstack VPC to support IPv4 and IPv6 network',
+    url: 'https://github.com/Klezca/dualstackVPC-ipv4-ipv6',
+    repo: 'https://github.com/Klezca/dualstackVPC-ipv4-ipv6', // if no repo, the button will not show up
   },
 ];
 
